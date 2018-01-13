606a2c77c07370d4aff85ec9f24e991777effc39
Niklaus Chi <nicholas.chi@aspectgaming.com>
2018-01-13 16:00:45 +0800

    add some function

---
+++ src/main/java/com/xiongmengyingshi/v17/utils/excel/ExcelException.java
@@ -0,0 +1,25 @@
+package com.xiongmengyingshi.v17.utils.excel;
+
+/**
+ * Created by nicholas.chi on 2018/1/13.
+ */
+public class ExcelException extends Exception {
+    public ExcelException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExcelException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExcelException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ExcelException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+}
